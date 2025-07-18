83672b7ac987379a347e00fbc81f8fe1a6a486d2
nacchan-kun <sendono.nc17@gmail.com>
2025-07-18 18:50:07 +0300

    Update page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
 } from '@tanstack/react-query';
 import { getNoteById } from '@/lib/api';
-import NoteDetailsClient from '@/app/notes/[id]/NotesDetails.client'
+import NoteDetailsClient from '@/app/notes/[id]/NoteDetails.client';
 
 type Props = {
   params: Promise<{ id: string }>;
